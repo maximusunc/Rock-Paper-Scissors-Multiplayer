1476b1b27953093464e1a08d770a4f6503d92bdf
Max Wang <maxwellswang@gmail.com>
2017-10-05 15:55:37 -0400

    added firebase function for only allowing 2 players

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,3 +10,27 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+
+var connectionsRef = database.ref("/connections");
+var connectedRef = database.ref(".info/connected");
+
+connectedRef.on("value", function(snap) {
+	if (snap.val()) {
+		var con = connectionsRef.push(true);
+		con.onDisconnect().remove();
+	}
+});
+
+connectionsRef.on("value", function(snap) {
+	$("#playerInfo").html("<input id=playerName type=text placeholder=Name><input id=enterPlayer type=submit>");
+	$("#enterPlayer").on("click", function() {
+		event.preventDefault();
+		if (snap.numChildren() == 1) {
+			$("#playerInfo1").html("Hi " + $("#playerName").val() + "! You are Player 1");
+		} else if (snap.numChildren() == 2) {
+			$("#playerInfo2").html("Hi " + $("#playerName").val() + "! You are Player 2");
+		} else {
+			$("#playerInfo").html("Sorry, two people are already playing");
+		};
+	});
+})
