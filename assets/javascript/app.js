9907c9ae6fb1e9d84ea3f6499843e645bcc41dc6
Max Wang <maxwellswang@gmail.com>
2017-10-05 14:36:10 -0400

    initailization of all files and bare bones

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBfUgyioCh73UJvvDgpe5vZsUnzijWtxRo",
+  authDomain: "rps-multiplayer-ea28f.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-ea28f.firebaseio.com",
+  projectId: "rps-multiplayer-ea28f",
+  storageBucket: "",
+  messagingSenderId: "413859234582"
+};
+firebase.initializeApp(config);
