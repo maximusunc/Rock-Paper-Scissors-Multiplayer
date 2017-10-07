ff05d46c23f04b4042e5769a77c1cc5f7c2a6745
Max Wang <maxwellswang@gmail.com>
2017-10-07 16:43:06 -0400

    fixed firebase child add function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,24 +13,40 @@ var database = firebase.database();
 
 var connectionsRef = database.ref("/connections");
 var connectedRef = database.ref(".info/connected");
+var addPlayer;
 
-connectedRef.on("value", function(snap) {
-	if (snap.val()) {
-		var con = connectionsRef.push(true);
-		con.onDisconnect().remove();
-	}
-});
-
-connectionsRef.on("value", function(snap) {
-	$("#playerInfo").html("<input id=playerName type=text placeholder=Name><input id=enterPlayer type=submit>");
-	$("#enterPlayer").on("click", function() {
+$("#playerInfo").html("<input id=playerName type=text placeholder=Name><input id=enterPlayer type=submit>");
+$("#enterPlayer").on("click", function() {
+	connectionsRef.once("value", function(snapshot){
 		event.preventDefault();
-		if (snap.numChildren() == 1) {
-			$("#playerInfo1").html("Hi " + $("#playerName").val() + "! You are Player 1");
-		} else if (snap.numChildren() == 2) {
-			$("#playerInfo2").html("Hi " + $("#playerName").val() + "! You are Player 2");
+		if (snapshot.numChildren() == 0){
+			console.log("test");
+			var con = connectionsRef.push({
+				player: $("#playerName").val(),
+			});
+			con.onDisconnect().remove();
+			$("#player1Info").html("Hi " + $("#playerName").val() + "! You are Player 1");
+			$("#playerInfo").empty();
+		} else if (snapshot.numChildren() == 1) {
+			console.log("test");
+			var con = connectionsRef.push({
+				player: $("#playerName").val(),
+			});
+			con.onDisconnect().remove();
+			$("#player2Info").html("Hi " + $("#playerName").val() + "! You are Player 2");
+			$("#playerInfo").empty();
 		} else {
-			$("#playerInfo").html("Sorry, two people are already playing");
+			console.log("test");
+			$("#player1Info").html("Sorry, two people are already playing");
 		};
 	});
-})
+	
+});
+
+var player1 = function() {
+
+};
+var player2 = function() {
+
+};
+
